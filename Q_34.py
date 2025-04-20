8b5760e1ed9c5402fa3e292b5e410481adb85f4e
Yaseen Naik <yaseen77m@hotmail.com>
2025-04-21 01:08:21 +0530

    Fixes #16 : Add solution to Question 34

---
+++ Q_34.py
@@ -0,0 +1,11 @@
+'''
+Author: Yaseen  
+Date: 13.04.2025  
+
+Q34. Write a Python program to check whether the string is Symmetrical or
+Palindrome
+'''
+def is_Palindrome(str):
+    return str==str[::-1]
+str1=input("Enter the String to check if Palindrome or not: ")
+print("String is Palindrome: ",is_Palindrome(str1))
