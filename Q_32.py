35e93dbd8be1d312d012e2e58e535a03e43a4fe9
FAB <143941135+FaheemorFAB@users.noreply.github.com>
2025-04-22 19:56:48 +0530

    Update Q_32.py

--- Q_32.py
+++ Q_32.py
@@ -1,3 +1,7 @@
+#Author: FAB  
+#Dated: 21st April, 2025
+#Q_32 Python program to sort a list of tuples by second Item
+
 t_list = [('a', 3), ('b', 1), ('c', 2)]
 
 # Sort by second item using lambda function
